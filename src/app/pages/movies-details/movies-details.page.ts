77a9329fea50713ca297a38c15629f303b8b351e
Dumbyarnik <62794996+Dumbyarnik@users.noreply.github.com>
2021-09-17 17:53:17 +0200

    Movie Details Page

--- src/app/pages/movies-details/movies-details.page.ts
+++ src/app/pages/movies-details/movies-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieSearchService } from 'src/app/services/movies-search/movie-search.service';
 
 @Component({
   selector: 'app-movies-details',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesDetailsPage implements OnInit {
 
-  constructor() { }
+  // variable to store info about the movie
+  information = null;
+
+  // ActivatedRoute we need for retrieving id
+  constructor(private activatedRoute: ActivatedRoute, 
+    private movieService: MovieSearchService) { }
 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.movieService.getDetails(id).subscribe(result =>{
+      console.log('details: ', result);
+      this.information = result;
+    });
+  }
+
+  // example of the button
+  openWebsite(){
+    window.open(this.information.homepage, '_blank');
   }
 
 }
