7c657e64dd07ee0112bcf074242e693c85892e09
Dumbyarnik <62794996+Dumbyarnik@users.noreply.github.com>
2021-09-18 10:13:21 +0200

    Created db and added a table

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatabaseService, User } from '../services/database/database.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  users: User[] = [];
+  user = {};
+
+  constructor(private db: DatabaseService) {}
+
+  ngOnInit(): void {
+    this.db.getDatabaseState().subscribe(rdy => {
+      if (rdy) {
+        this.db.getUsers().subscribe(users => {
+          this.users = users;
+        })
+      }
+    });
+  }
 
 }
