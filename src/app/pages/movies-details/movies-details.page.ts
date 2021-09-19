8cf0f8b75f928efd5dee96f3c1050423399f6a57
Dumbyarnik <62794996+Dumbyarnik@users.noreply.github.com>
2021-09-19 12:06:41 +0200

    Save Movie to Watch List

--- src/app/pages/movies-details/movies-details.page.ts
+++ src/app/pages/movies-details/movies-details.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/api/api.service';
+import { Storage } from '@capacitor/storage';
 
 
 @Component({
@@ -25,9 +26,21 @@ export class MoviesDetailsPage implements OnInit {
     });
   }
 
-  // example of the button
+  // button - open website
   openWebsite(){
     window.open(this.information.homepage, '_blank');
+  }
+
+  // button - save to watchlist
+  async saveToWatch(){
+    var user = await Storage.get({ key: 'user'});
+    var data = JSON.parse(user.value);
+    data[0].movies_want.push(this.information.id);
+    
+    await Storage.set({
+      key: 'user',
+      value: JSON.stringify(data),
+    });
   }
 
 }
